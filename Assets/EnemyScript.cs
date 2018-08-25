827da6ea9a69cc323dca34d404ae39e7b1c62313
dhoso <34808983+dhoso@users.noreply.github.com>
2018-08-25 16:15:11 +0900

    Adjust movenent of enemy

--- Assets/EnemyScript.cs
+++ Assets/EnemyScript.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class EnemyScript : MonoBehaviour {
 	public Transform player;
 	private Rigidbody rbody;
-	private float speed = 3f;
+	private float speed = 30f;
 
 	// Use this for initialization
 	void Start () {
@@ -14,7 +14,8 @@ public class EnemyScript : MonoBehaviour {
 
 	// Update is called once per frame
 	void FixedUpdate () {
-		Vector3 diff = player.position - this.transform.position;
-		rbody.AddForce(diff * speed);
+		Vector3 direction = Vector3.Normalize(player.position - this.transform.position);
+		direction.y = 0f;
+		rbody.AddForce(direction * speed);
 	}
 }
