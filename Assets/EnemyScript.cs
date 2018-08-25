3906f83ae67cf6f4a8daa9b620ea0580bd338c56
dhoso <34808983+dhoso@users.noreply.github.com>
2018-08-25 15:59:36 +0900

    Add Enemy

---
+++ Assets/EnemyScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyScript : MonoBehaviour {
+	public Transform player;
+	private Rigidbody rbody;
+	private float speed = 3f;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody>();
+	}
+
+	// Update is called once per frame
+	void FixedUpdate () {
+		Vector3 diff = player.position - this.transform.position;
+		rbody.AddForce(diff * speed);
+	}
+}
