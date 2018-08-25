f963a0fb3b569ef6d7915123722bc351f282ec85
dhoso <34808983+dhoso@users.noreply.github.com>
2018-08-25 15:28:13 +0900

    Develop key controll of Player

---
+++ Assets/CameraScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour {
+	public Transform target;
+	public Vector3 relativePosition;
+
+	void Start () {
+		GetComponent<Transform>().position = target.position + relativePosition;
+	}
+
+	void Update () {
+		GetComponent<Transform>().position = target.position + relativePosition;
+	}
+}
---
+++ Assets/PlayerScript.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerScript : MonoBehaviour {
+	public Rigidbody rbody;
+	private float speed = 30f;
+
+	// Use this for initialization
+	void Start () {
+		rbody = GetComponent<Rigidbody>();
+	}
+
+	void FixedUpdate () {
+		float x = Input.GetAxis("Horizontal") * speed;
+		float z = Input.GetAxis("Vertical") * speed;
+		rbody.AddForce(x, 0, z);
+	}
+}
